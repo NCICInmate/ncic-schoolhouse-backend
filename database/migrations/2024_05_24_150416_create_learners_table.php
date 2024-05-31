5e05553d47f78588f6aa44d3f39a5ac5d52f4871
Marshall Smith <marshallksmith16@gmail.com>
2024-05-31 15:48:05 -0500

    add database migration for learner profile

---
+++ database/migrations/2024_05_24_150416_create_learners_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('learners', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->timestamps();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->mediumText('motivation')->nullable();
+            $table->date('release_date')->nullable();
+            $table->longText('accomplishments')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('learners');
+    }
+};
